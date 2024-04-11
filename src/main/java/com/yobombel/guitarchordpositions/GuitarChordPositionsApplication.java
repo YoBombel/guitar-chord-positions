898b170db6cc7506df288122bb4a1db27c749612
BienkowskiTomasz <it.bienkowski@gmail.com>
2024-04-11 12:01:27 +0200

    Initial commit

---
+++ src/main/java/com/yobombel/guitarchordpositions/GuitarChordPositionsApplication.java
@@ -0,0 +1,13 @@
+package com.yobombel.guitarchordpositions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarChordPositionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuitarChordPositionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yobombel/guitarchordpositions/GuitarChordPositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yobombel.guitarchordpositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarChordPositionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
